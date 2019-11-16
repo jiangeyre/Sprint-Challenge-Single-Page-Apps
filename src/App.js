31f0ec62b37c90dc9175696fbbb1661a57f7c582
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-11-15 18:30:55 -0800

    this netlify thing is messing with my head

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
 import SearchFrom from "./components/SearchForm";
@@ -11,19 +11,6 @@ export default function App() {
   return (
     <Router>
       <main>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/characters">Characters</Link>
-            </li>
-            <li>
-              <Link to="/search">Search</Link>
-            </li>
-          </ul>
-        </nav>
         <Header />
         <Route exact path="/" component={WelcomePage} />
         <Route exact path="/characters" component={CharacterList} />
